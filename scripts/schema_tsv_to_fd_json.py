e877b5a46022a80b81ac6ba26277336b19834b2d
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-11-28 15:55:44 -0700

    Add other util scripts

--- scripts/schema_tsv_to_fd_json.py
+++ scripts/schema_tsv_to_fd_json.py
@@ -6,44 +6,56 @@ Read Kai's Schema TSV from stdin and output Frictionless Data Table Schema JSON 
 import sys
 import json
 
-header = sys.stdin.readline()
-cols = [t.strip() for t in header.split(sep='\t')]
+# header = sys.stdin.readline()
+# cols = [t.strip() for t in header.split(sep='\t')]
+#
+# obj = {
+#     '@context': {
+#         'pm': 'http://planetmicrobe.org/rdf/'
+#     },
+#     'profile': 'tabular-data-package',
+#     'name': '',
+#     'title': '',
+#     'homepage': '',
+#     'licenses': [],
+#     'resources': [{
+#         'name': '',
+#         'title': '',
+#         'profile': 'tabular-data-resource',
+#         'schema': {
+#             'fields': [],
+#             'missingValues': [ '' ]
+#         }
+#     }]
+# }
+#
+# for l in sys.stdin:
+#     inp = {}
+#     for t, f in zip(cols, l.split(sep='\t')):
+#         inp[t.strip()] = f.strip()
+#
+#     field = {
+#         'name': inp['Short Name'],
+#         'title': inp['Parameter'],
+#         'type': 'string' if not inp['Unit'] else 'number', # assume number if unit is specified
+#         'format': 'default',
+#         'description': inp['Comment'] + (', ' + inp['Method'] if inp['Method'] else ''),
+#         'constraints': { 'required': True },
+#         'rdfType': inp['PURL/TEMP PURL'],
+#         'pm:unitOfMeasure': inp['Unit']
+#     }
+#     obj['resources'][0]['schema']['fields'].append(field)
+#
+# print(json.dumps(obj, indent=4))
 
-obj = {
-    '@context': {
-        'pm': 'http://planetmicrobe.org/rdf/'
-    },
-    'profile': 'tabular-data-package',
-    'name': '',
-    'title': '',
-    'homepage': '',
-    'licenses': [],
-    'resources': [{
-        'name': '',
-        'title': '',
-        'profile': 'tabular-data-resource',
-        'schema': {
-            'fields': [],
-            'missingValues': [ '' ]
-        }
-    }]
-}
+from tableschema import Table
 
-for l in sys.stdin:
-    inp = {}
-    for t, f in zip(cols, l.split(sep='\t')):
-        inp[t.strip()] = f.strip()
+try:
+    table = Table(sys.argv[1])
+except Exception as e:
+    print(e)
 
-    field = {
-        'name': inp['Short Name'],
-        'title': inp['Parameter'],
-        'type': 'string' if not inp['Unit'] else 'number', # assume number if unit is specified
-        'format': 'default',
-        'description': inp['Comment'] + (', ' + inp['Method'] if inp['Method'] else ''),
-        'constraints': { 'required': True },
-        'rdfType': inp['PURL/TEMP PURL'],
-        'pm:unitOfMeasure': inp['Unit']
-    }
-    obj['resources'][0]['schema']['fields'].append(field)
-
-print(json.dumps(obj, indent=4))
+# print("Headers:", table.headers)
+# print(table.read(keyed=True))
+table.infer()
+print(json.dumps(table.schema.descriptor, indent=2))
