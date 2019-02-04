8de2032ec8ee5cf410de95d902574f1c544678fd
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-02-04 15:01:39 -0700

    Remove old relational schema

--- scripts/postgres.sql
+++ scripts/postgres.sql
@@ -24,20 +24,3 @@ CREATE TABLE sample (
 CREATE INDEX schema_id_idx ON sample (schema_id);
 
 CREATE INDEX location_gix ON sample USING GIST (location);
-
-
---CREATE TABLE sample (
---    sample_id serial PRIMARY KEY,
---    schema_id integer NOT NULL REFERENCES schema(schema_id)
---);
---
---CREATE TABLE field (
---    field_id serial PRIMARY KEY,
---    schema_id integer NOT NULL REFERENCES schema(schema_id),
---    sample_id integer NOT NULL REFERENCES sample(sample_id),
---    field_num integer NOT NULL,
---    string_value text,
---    number_value real
---);
---
---CREATE INDEX ON field (schema_id, field_num);
