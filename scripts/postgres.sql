67cb452acdc9fba02d0d34951fa6a2db1b053d4d
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-02-15 14:10:39 -0700

    Change field types to uppercase

--- scripts/postgres.sql
+++ scripts/postgres.sql
@@ -7,18 +7,23 @@ CREATE DATABASE arraytestgis;
 CREATE EXTENSION Postgis;
 
 CREATE TABLE schema (
-    schema_id serial PRIMARY KEY,
-    name varchar(100) NOT NULL,
-    fields json NOT NULL
+    schema_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    fields JSON NOT NULL
 );
 
 CREATE TABLE sample (
-    sample_id serial PRIMARY KEY,
-    schema_id integer NOT NULL REFERENCES schema(schema_id),
-    location geography(point,4326),
-    number_vals real [],
-    string_vals text [],
-    datetime_vals timestamp []
+    sample_id SERIAL PRIMARY KEY,
+    schema_id INTEGER NOT NULL REFERENCES schema(schema_id),
+    sample_accn VARCHAR(255)
+    creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    location GEOGRAPHY(POINT,4326),
+
+    -- Fields for storing dataset-specific attributes
+    number_vals REAL [],
+    string_vals TEXT [],
+    datetime_vals TIMESTAMP []
 );
 
 CREATE INDEX schema_id_idx ON sample (schema_id);
