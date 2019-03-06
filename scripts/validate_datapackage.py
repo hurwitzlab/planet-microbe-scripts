44a5e9179ce2e4fefefc008b77cbf853bee89e81
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-03-06 15:56:30 -0700

    Remove unused code

--- scripts/validate_datapackage.py
+++ scripts/validate_datapackage.py
@@ -29,45 +29,18 @@ if __name__ == "__main__":
     print('Resources: ', package.resource_names)
 
     for rname in package.resource_names:
-        schema_name = package.descriptor['name'] + '-' + rname
         resource = package.get_resource(rname)
-        print('Validating schema ...')
+        print('Validating resource', rname)
         schema = resource.schema
 
-        count = 0
+        totalCount = 0
         try:
             for row in resource.read():
-                print(row)
-
-    #             for val in row:
-    #                 type = schema.fields[i].type
-    #                 rdfType = schema.descriptor['fields'][i]['rdfType']
-    #                 if type == 'number':
-    #                     if val == None: # no data
-    #                         numberVals.append(None)
-    #                     else:
-    #                         numberVals.append(float(val))
-    #                         if rdfType == "http://purl.obolibrary.org/obo/OBI_0001620":
-    #                             latitude = float(val)
-    #                         elif rdfType == "http://purl.obolibrary.org/obo/OBI_0001621":
-    #                             longitude = float(val)
-    #                     stringVals.append(None)
-    #                     datetimeVals.append(None)
-    #                 elif type == 'string':
-    #                     stringVals.append(str(val))
-    #                     numberVals.append(None)
-    #                     datetimeVals.append(None)
-    #                 elif type == 'datetime': #TODO handle time zone
-    #                     stringVals.append(None)
-    #                     numberVals.append(None)
-    #                     datetimeVals.append(str(val))
-    #                 else:
-    #                     print("Unknown type:", type)
-    #                     exit(-1)
-    #                 i += 1
-    #
-    #             count += 1
+                totalCount += 1
         except Exception as e:
             print(e)
             if e.errors:
                 print(*e.errors, sep='\n')
+
+        print("Total rows:", totalCount)
+        print("Resource", rname, "is valid")
