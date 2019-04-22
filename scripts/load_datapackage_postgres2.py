f2cd1f805fb135ecd6db2b5f23390f74eb5b2c0b
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-04-22 14:07:31 -0700

    Add loading of project and project_to_sample tables

--- scripts/load_datapackage_postgres2.py
+++ scripts/load_datapackage_postgres2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Load Data Package schema and data into Postgres
+Load Data Package schema and data into Postgres database
 """
 
 import sys
@@ -28,6 +28,7 @@ CAMPAIGN_CRUISE_DB_SCHEMA = {
 }
 
 SAMPLING_EVENT_DB_SCHEMA = {
+    "sampling_event_id": "http://purl.obolibrary.org/obo/PMO_00000056",
     "sampling_event_type": "http://purl.obolibrary.org/obo/pmo.owl/PMO_00000146",
     "campaign_id": "http://purl.obolibrary.org/obo/PMO_00000060",
     "latitude": "http://purl.obolibrary.org/obo/OBI_0001621",
@@ -51,14 +52,6 @@ LONGITUDE_PURLS = [
     "http://purl.obolibrary.org/obo/PMO_00000077",
     "http://purl.obolibrary.org/obo/PMO_00000078"
 ]
-
-
-# def db_create_schema(fdSchema):
-#     #TODO check if schema already exists
-#     schemaId = cursor.execute('INSERT INTO schema (name) VALUES (%s) RETURNING schema_id;', [schema_name])
-#
-#     for f in fdSchema.fields:
-#         cursor.execute('INSERT INTO field ...
 
 
 def get_resources_by_type(type, resources):
@@ -91,19 +84,22 @@ def load_resource(db, resource, dbSchema, tableName, insertMethod):
 
         fieldMap[k] = fields
 
+    results = {}
     try:
         for row in resource.read(keyed=True):
             #print(row)
             obj = {}
             for k,fields in fieldMap.items():
                 vals = list(filter(lambda v: v != None, map(lambda f: row[f.name], fields)))
-                #print(k, ":", vals)
                 obj[k] = vals
-            insertMethod(db, tableName, obj)
+            id = insertMethod(db, tableName, obj)
+            results[id] = obj
     except Exception as e:
         print(e)
         if e.errors:
             print(*e.errors, sep='\n')
+
+    return results
 
 
 def load_campaigns(db, package):
@@ -114,52 +110,55 @@ def load_campaigns(db, package):
         raise Exception("More than one campaign resource found")
 
     load_resource(db, campaigns[0], CAMPAIGN_CRUISE_DB_SCHEMA, "campaign", insert_campaign)
+    db.commit()
 
 
 def insert_campaign(db, tableName, obj):
     cursor = db.cursor()
     cursor.execute(
-        'INSERT INTO campaign (campaign_type,name,deployment,start_location,end_location,start_time,end_time,urls) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);',
+        'INSERT INTO campaign (campaign_type,name,deployment,start_location,end_location,start_time,end_time,urls) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) RETURNING campaign_id',
         ['cruise', obj['name'][0], obj['deployment'][0], obj['start_location'][0], obj['end_location'][0], obj['start_time'][0], obj['end_time'][0], obj['urls']]
     )
-    db.commit()
+    return cursor.fetchone()[0]
 
 
-def load_sampling_events(db, package):
+def load_sampling_events(db, package, campaigns):
     events = get_resources_by_type("sampling_event", package.resources)
     if not events:
         raise Exception("No sampling_event resource found")
     elif len(events) > 1:
         raise Exception("More than one sampling_event resource found")
 
-    load_resource(db, events[0], SAMPLING_EVENT_DB_SCHEMA, "sampling_event", insert_sampling_event)
+    entries = load_resource(db, events[0], SAMPLING_EVENT_DB_SCHEMA, "sampling_event", insert_sampling_event)
+    db.commit()
+    return entries
 
 
 def insert_sampling_event(db, tableName, obj):
     cursor = db.cursor()
     cursor.execute('SELECT campaign_id FROM campaign WHERE name=%s', obj['campaign_id']) # FIXME campaign name may not be unique
     campaign_id = cursor.fetchone()[0]
-    #print("campaign_id: ", campaign_id)
     cursor.execute(
-        'INSERT INTO sampling_event (sampling_event_type,campaign_id,locations,start_time,data_url) VALUES (%s,%s,ST_SetSRID(ST_MakeLine(ARRAY[ST_MakePoint(%s,%s)]),4326),%s,%s);',
+        'INSERT INTO sampling_event (sampling_event_type,campaign_id,locations,start_time,data_url) VALUES (%s,%s,ST_SetSRID(ST_MakeLine(ARRAY[ST_MakePoint(%s,%s)]),4326),%s,%s) RETURNING sampling_event_id',
         [obj['sampling_event_type'][0], campaign_id, obj['longitude'][0], obj['latitude'][0], obj['start_time'][0], "FIXME"]
     )
-    db.commit()
+    return cursor.fetchone()[0]
 
 
-def load_samples(db, package):
+def load_samples(db, package, sampling_events):
     resources = get_resources_by_type("sample", package.resources)
     if not resources:
         raise Exception("No sample resources found")
 
+    # Join schema and data for all sample resources
     allFields = []
     valuesBySampleId = {}
-
+    sampleIdToSampleEventId = {}
     for i in range(len(resources)):
         resource = resources[i]
         print("Sample resource", resource.name)
 
-        # First, determine position of Sample Event ID and Sample ID fields
+        # Determine position of Sample Event ID and Sample ID fields
         fields = resource.schema.descriptor['fields']
         sampleIdPos = list(map(lambda f: f['rdfType'], fields)).index(SAMPLE_ID_PURL)
         sampleEventIdPos = list(map(lambda f: f['rdfType'], fields)).index(SAMPLE_EVENT_ID_PURL)
@@ -170,20 +169,24 @@ def load_samples(db, package):
         allFields.extend(fields)
 
         for row in resource.read():
-            id = row[sampleIdPos]
+            sampleId = row[sampleIdPos]
+            sampleEventId = row[sampleEventIdPos]
+            sampleIdToSampleEventId[sampleId] = sampleEventId
+
             if i > 0:
                 del row[sampleIdPos]
                 del row[sampleEventIdPos]
 
-            if not id in valuesBySampleId:
-                valuesBySampleId[id] = []
-            valuesBySampleId[id].extend(row)
+            if not sampleId in valuesBySampleId:
+                valuesBySampleId[sampleId] = []
+            valuesBySampleId[sampleId].extend(row)
 
     # Load schema
     schema_id = insert_schema(db, package.descriptor['name'], { "fields": allFields })
 
     # Load sample values
     cursor = db.cursor()
+    samples = {}
     count = 0
     for id in valuesBySampleId:
         latitudeVals = []
@@ -226,30 +229,71 @@ def load_samples(db, package):
                 exit(-1)
             i += 1
 
+        sampling_event_id = None
+        for eventId in sampling_events:
+            if sampling_events[eventId]['sampling_event_id'][0] == sampleIdToSampleEventId[id]:
+                sampling_event_id = eventId
+                break
+
         locations = MultiPoint(list(zip(longitudeVals, latitudeVals)))
+
         stmt = cursor.mogrify(
-            "INSERT INTO sample (schema_id,locations,number_vals,string_vals,datetime_vals) VALUES(%s,ST_SetSRID(%s::geography, 4326),%s,%s,%s::timestamp[])",
-            [schema_id, locations.wkb_hex, numberVals, stringVals, datetimeVals]
+            "INSERT INTO sample (schema_id,sampling_event_id,locations,number_vals,string_vals,datetime_vals) VALUES(%s,%s,ST_SetSRID(%s::geography, 4326),%s,%s,%s::timestamp[]) RETURNING sample_id",
+            [schema_id, sampling_event_id, locations.wkb_hex, numberVals, stringVals, datetimeVals]
         )
         cursor.execute(stmt)
+        sample_id = cursor.fetchone()[0]
+        samples[sample_id] = valuesBySampleId[id]
         db.commit()
         count += 1
         print('\rLoading samples', count, end='')
     print()
 
+    return samples
+
 
 def insert_schema(db, name, schema):
     cursor = db.cursor()
     print('Loading schema "%s"' % name)
-    cursor.execute('INSERT INTO schema (name,fields) VALUES (%s,%s) RETURNING schema_id;',
+    cursor.execute('INSERT INTO schema (name,fields) VALUES (%s,%s) RETURNING schema_id',
                    [name, json.dumps(schema)])
     schema_id = cursor.fetchone()[0]
     db.commit()
-    print("schema_id:", schema_id)
+    print("Added schema", schema_id)
     return schema_id
 
 
-#def insert_sample(db, tableName, obj):
+def insert_project(db, package, samples):
+    cursor = db.cursor()
+
+    # Create project_type entry
+    type = package.descriptor['pm:projectType']
+
+    if not type:
+        raise Exception("Missing pm:projectType field")
+
+    cursor.execute('SELECT project_type_id FROM project_type WHERE name=%s', [type])
+    if cursor.rowcount == 0:
+        cursor.execute('INSERT INTO project_type (name) VALUES (%s) RETURNING project_type_id', [type])
+    project_type_id = cursor.fetchone()[0]
+
+    # Create project entry
+    title = package.descriptor['title']
+    description = package.descriptor['description']
+    homepage = package.descriptor['homepage']
+
+    cursor.execute(
+        'INSERT INTO project (project_type_id,accn,name,description,url) VALUES (%s,%s,%s,%s,%s) RETURNING project_id',
+        [project_type_id, "foo", title, description, homepage]
+    )
+    project_id = cursor.fetchone()[0]
+    print("Added project", project_id)
+
+    # Create project_to_sample entries
+    for sampleId in samples:
+        cursor.execute('INSERT INTO project_to_sample (project_id,sample_id) VALUES (%s,%s)', [project_id, sampleId])
+
+    db.commit()
 
 
 def main(args=None):
@@ -261,76 +305,10 @@ def main(args=None):
     if not package.valid:
         print(package.errors)
 
-    load_campaigns(conn, package)
-    load_sampling_events(conn, package)
-    load_samples(conn, package)
-    exit()
-
-    print('Resources: ', package.resource_names)
-
-    for rname in package.resource_names:
-        if args['resource'] != None and rname != args['resource']:
-            continue
-
-        schema_name = package.descriptor['name'] + '-' + rname
-        resource = package.get_resource(rname)
-        print('Loading schema "%s"' % schema_name)
-        schema = resource.schema
-        cursor.execute('INSERT INTO schema (name,fields) VALUES (%s,%s) RETURNING schema_id;', [schema_name, json.dumps(schema.descriptor)])
-        schema_id = cursor.fetchone()[0]
-        conn.commit()
-        print("schema_id:", schema_id)
-
-        count = 0
-        try:
-            for row in resource.read():#(relations=True, extended=True):
-                # Array Implementation
-                latitude = None
-                longitude = None
-                numberVals = []
-                stringVals = []
-                datetimeVals = []
-                i = 0
-                for val in row:
-                    type = schema.fields[i].type
-                    rdfType = schema.descriptor['fields'][i]['rdfType']
-                    if type == 'number':
-                        if val == None: # no data
-                            numberVals.append(None)
-                        else:
-                            numberVals.append(float(val))
-                            if rdfType == "http://purl.obolibrary.org/obo/OBI_0001620":
-                                latitude = float(val)
-                            elif rdfType == "http://purl.obolibrary.org/obo/OBI_0001621":
-                                longitude = float(val)
-                        stringVals.append(None)
-                        datetimeVals.append(None)
-                    elif type == 'string':
-                        stringVals.append(str(val))
-                        numberVals.append(None)
-                        datetimeVals.append(None)
-                    elif type == 'datetime' or type == 'date': #TODO handle time zone
-                        stringVals.append(None)
-                        numberVals.append(None)
-                        datetimeVals.append(val)
-                    else:
-                        print("Unknown type:", type)
-                        exit(-1)
-                    i += 1
-
-                stmt = cursor.mogrify(
-                    "INSERT INTO sample (schema_id,locations,number_vals,string_vals,datetime_vals) VALUES(%s,ST_SetSRID(ST_MakeLine(ARRAY[ST_MakePoint(%s,%s)]),4326),%s,%s,%s::timestamp[])",
-                    [schema_id,longitude,latitude,numberVals,stringVals,datetimeVals]
-                )
-                cursor.execute(stmt)
-                conn.commit()
-                count += 1
-                print('\rLoading resource "%s" ...' % rname, count, end='')
-            print()
-        except Exception as e:
-            print(e)
-            if e.errors:
-                print(*e.errors, sep='\n')
+    campaigns = load_campaigns(conn, package)
+    sampling_events = load_sampling_events(conn, package, campaigns)
+    samples = load_samples(conn, package, sampling_events)
+    insert_project(conn, package, samples)
 
 
 if __name__ == "__main__":
