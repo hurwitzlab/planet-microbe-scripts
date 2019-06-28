292074d4e39b6572861aa60016f192e317b27740
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-06-28 14:17:24 -0700

    Add args

--- scripts/load_all_datapackages.sh
+++ scripts/load_all_datapackages.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+USERNAME=$1
+DBNAME=$2
+PASSWORD=$3
 DELETEALL="-x"
 
 for path in `ls -d ../../planet-microbe-datapackages/*`
 do
-    if test -f "$path/datapackage.json"; then
-        ./load_datapackage_postgres2.py $DELETEALL -u mbomhoff -d pm_test $path/datapackage.json
+    file=$path/datapackage.json
+    if test -f $file; then
+        ./load_datapackage_postgres2.py $DELETEALL -u $USERNAME -d $DBNAME -p $PASSWORD $file
     fi
     DELETEALL=""
 done
