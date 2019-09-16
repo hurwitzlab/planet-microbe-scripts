bad4f3bf049f40c8decf3fdd475164731c0b0b54
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-09-16 16:43:43 -0700

    Add alternative method for finding SRA accn

--- scripts/load_experiments.py
+++ scripts/load_experiments.py
@@ -39,7 +39,9 @@ def getExperimentsFromSRA(sampleAccn):
         #     print(record)
 
         record = ET.fromstring(summary['SampleData'])
-        attr = record.find(".//Attribute[@attribute_name='SRA accession']") #record.find(".//Id[@db='SRA']")
+        attr = record.find(".//Attribute[@attribute_name='SRA accession']")
+        if attr == None:
+            attr = record.find(".//Id[@db='SRA']")
         if attr == None:
             print(summary['SampleData'])
             raise Exception("Could not parse SRA accn for BioSample:", sampleAccn)
