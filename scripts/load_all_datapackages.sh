abef70fa43fe614ca5943cf7354887cdb53e2728
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-10-28 10:35:37 -0700

    Add arg for irods path

--- scripts/load_all_datapackages.sh
+++ scripts/load_all_datapackages.sh
@@ -4,13 +4,14 @@ set -e
 USERNAME="$1"
 DBNAME="$2"
 PASSWORD="$3"
+IRODSPATH="$4"
 DELETEALL="-x"
 
 for path in `ls -d ../../planet-microbe-datapackages/*`
 do
     file=$path/datapackage.json
     if test -f $file; then
-        ./load_datapackage_postgres2.py $DELETEALL -u $USERNAME -d $DBNAME -p $PASSWORD -i /iplant/home/shared/planetmicrobe/ctd_and_niskin $file
+        ./load_datapackage_postgres2.py $DELETEALL -u $USERNAME -d $DBNAME -p $PASSWORD -i $IRODSPATH $file
     fi
     DELETEALL=""
 done
