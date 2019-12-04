a4f27e4028010d7a2b0202b979129f9314c19585
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-12-04 10:02:32 -0700

    Hardcode list of data packages to load

--- scripts/load_all_datapackages.sh
+++ scripts/load_all_datapackages.sh
@@ -3,15 +3,26 @@ set -e
 
 USERNAME="$1"
 DBNAME="$2"
-PASSWORD="$3"
-IRODSPATH="$4"
+PASSWORD=$3
+if test -n "$PASSWORD"; then PASSWORD="-p $PASSWORD"; fi
+IRODSPATH=$4
+if test -n "$IRODSPATH"; then IRODSPATH="-i $IRODSPATH"; fi
 DELETEALL="-x"
+DATAPACKAGES="\
+../../planet-microbe-datapackages/GOS_2009-10 \
+../../planet-microbe-datapackages/HOT-Chisholm \
+../../planet-microbe-datapackages/HOT-DeLong \
+../../planet-microbe-datapackages/HOT_Delong_metatranscriptomes \
+../../planet-microbe-datapackages/OSD \
+../../planet-microbe-datapackages/Tara_Oceans"
 
-for path in `ls -d ../../planet-microbe-datapackages/*`
+
+#for path in `ls -d ../../planet-microbe-datapackages/*`
+for path in $DATAPACKAGES
 do
     file=$path/datapackage.json
     if test -f $file; then
-        ./load_datapackage_postgres2.py $DELETEALL -u $USERNAME -d $DBNAME -p $PASSWORD -i $IRODSPATH $file
+        ./load_datapackage_postgres2.py $DELETEALL -u $USERNAME -d $DBNAME $PASSWORD $IRODSPATH $file
+        DELETEALL=""
     fi
-    DELETEALL=""
 done
