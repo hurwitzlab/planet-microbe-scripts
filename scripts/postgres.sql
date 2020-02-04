a6f6f88c345ba27a1753a36edc5010c6f9227568
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-02-04 13:37:29 -0700

    Cleanup

--- scripts/postgres.sql
+++ scripts/postgres.sql
@@ -1,55 +1,13 @@
--- Load:
 -- psql -d <db_name> -f postgres.sql
--- delete from project_to_sample; delete from sample; delete from project; delete from schema; delete from sampling_event; delete from campaign;
 
 CREATE EXTENSION Postgis;
-
---CREATE TABLE ontology (
---    ontology_id SERIAL PRIMARY KEY,
---    name VARCHAR(255) UNIQUE NOT NULL,
---    description TEXT,
---    purl TEXT UNIQUE NOT NULL
---);
-
---CREATE TABLE ontology_term (
---    ontology_term_id SERIAL PRIMARY KEY,
---    ontology_id INTEGER NOT NULL REFERENCES ontology(ontology_id),
---    label VARCHAR(255) NOT NULL,
---    description TEXT NOT NULL,
---    purl TEXT UNIQUE NOT NULL
---);
 
 CREATE TABLE schema (
     schema_id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    fields JSON NOT NULL, --TODO remove, replaced by field tables
+    fields JSON NOT NULL,
     creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
---CREATE TABLE field_source_category (
---    field_source_category_id SERIAL PRIMARY KEY,
---    name VARCHAR(255) UNIQUE NOT NULL,
---    description TEXT
---);
---
---CREATE TABLE field_source (
---    field_source_id SERIAL PRIMARY KEY,
---    field_source_category_id INTEGER NOT NULL REFERENCES field_source_category(field_source_category_id),
---    url TEXT UNIQUE NOT NULL
---);
---
---CREATE TYPE field_type AS ENUM ('number', 'string', 'datetime');
---
---CREATE TABLE field (
---    field_id SERIAL PRIMARY KEY,
---    field_source_id INTEGER NOT NULL REFERENCES field_source(field_source_id),
---    schema_id INTEGER NOT NULL REFERENCES schema(schema_id),
-----    ontology_term_id INTEGER NOT NULL REFERENCES ontology_term(ontology_term_id),
-----    unit_ontology_term_id INTEGER NOT NULL REFERENCES ontology_term(ontology_term_id),
---    name VARCHAR(255) NOT NULL,
---    type field_type NOT NULL,
---    position INTEGER NOT NULL
---);
 
 CREATE TABLE campaign_type (
     campaign_type_id SERIAL PRIMARY KEY,
@@ -57,7 +15,7 @@ CREATE TABLE campaign_type (
     description TEXT
 );
 
--- Campaign represents cruise
+-- Campaign represents a cruise
 CREATE TABLE campaign (
     campaign_id SERIAL PRIMARY KEY,
 --    campaign_type_id INTEGER NOT NULL REFERENCES campaign_type(campaign_type_id),
@@ -91,8 +49,6 @@ CREATE TABLE sampling_event (
 --    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
---CREATE INDEX sampling_event_locations_gix ON sample USING GIST (locations);
-
 CREATE TABLE sample (
     sample_id SERIAL PRIMARY KEY,
     schema_id INTEGER NOT NULL REFERENCES schema(schema_id),
@@ -106,18 +62,6 @@ CREATE TABLE sample (
     creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 --    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
-
---CREATE INDEX sample_locations_gix ON sample USING GIST (locations);
-
---CREATE TABLE sample_data (
---    sample_id INTEGER NOT NULL REFERENCES sample(sample_id),
---    purl VARCHAR(255) NOT NULL,
---    type VARCHAR(255) NOT NULL,
---    number_vals REAL [],
---    string_vals TEXT [],
---    datetime_vals TIMESTAMP [],
---    UNIQUE(sample_id, purl, type)
---);
 
 CREATE TABLE sample_to_sampling_event (
     sample_to_sampling_event_id SERIAL PRIMARY KEY,
@@ -151,16 +95,6 @@ CREATE TABLE project_to_sample (
     sample_id INTEGER NOT NULL REFERENCES sample(sample_id),
     UNIQUE(project_id, sample_id)
 );
-
--- replaces project_group in iMicrobe
---CREATE TABLE organization (
---    organization_id SERIAL PRIMARY KEY,
---    name VARCHAR(255) UNIQUE NOT NULL,
---    description TEXT,
---    url VARCHAR(255),
---    private BOOLEAN NOT NULL DEFAULT TRUE,
---    creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
---);
 
 CREATE TABLE experiment (
     experiment_id SERIAL PRIMARY KEY,
@@ -284,16 +218,4 @@ CREATE TABLE app_run (
 --  app_id INTEGER NOT NULL REFERENCES app(app_id),
 --  app_data_type_id INTEGER unsigned NOT NULL,
 --  path VARCHAR(255) DEFAULT NULL
---);
-
---CREATE TABLE organization_to_user (
---    organization_to_user_id SERIAL PRIMARY KEY,
---    organization_id INTEGER NOT NULL REFERENCES organization(organization_id),
---    user_id INTEGER NOT NULL REFERENCES "user"(user_id)
---);
---
---CREATE TABLE organization_to_project (
---    organization_to_project_id SERIAL PRIMARY KEY,
---    organization_id INTEGER NOT NULL REFERENCES organization(organization_id),
---    project_id INTEGER NOT NULL REFERENCES project(project_id)
 --);
